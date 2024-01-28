8e3bda5cc9ffd75f96795dd0a5b05d39076d2273
Krab_s_Marsa <c0oler9911@yandex.ru>
2024-01-28 19:08:05 +0300

    now

--- src/ru/progwards/java1/lessons/wrappers/StringConverter.java
+++ src/ru/progwards/java1/lessons/wrappers/StringConverter.java
@@ -2,34 +2,38 @@ package ru.progwards.java1.lessons.wrappers;
 
 public class StringConverter {
     public static String fromByte(byte number){
-        Byte num = Byte.parseByte(fromByte(number));
-        String a = num.toString();
-        String fromByte = fromByte(number);
-        return fromByte;
-    }
-    public static String fromShort(long number){
-        String b = fromShort(number);
-        return b;
-    }
-    public static String fromInt(int number){
-        String c = fromInt(number);
+        Byte numa = number;
+        String c = numa.toString();
+        System.out.println(c);
         return c;
     }
+    public static String fromShort(short number){
+        Short numa = number;
+        String c = numa.toString();
+        System.out.println(c);
+        return c;
+    }
+    public static String fromInt(int number){
+        Integer numa = number;
+        String c = numa.toString();
+        System.out.println(c);
+        return c;}
     public static String fromLong(long number){
-        String d = fromLong(number);
-        return d;
+        Long numa = number;
+        String c = numa.toString();
+        System.out.println(c);
+        return c;
     }
     public static String fromFloat(float number){
-        String e = fromFloat(number);
-        return e;
+        Float numa = number;
+        String c = numa.toString();
+        System.out.println(c);
+        return c;
     }
     public static String fromDouble(double number){
-        String f = fromDouble(number);
-        return f;
-}
-
-    public static void main(String[] args) {
-        System.out.println(fromDouble(1.0));
+        Double numa = number;
+        String c = numa.toString();
+        System.out.println(c);
+        return c;
     }
-
 }
