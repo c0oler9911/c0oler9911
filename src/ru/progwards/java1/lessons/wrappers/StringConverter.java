4c78d5b29305702cfacfcff0487634ac9263a2b5
Krab_s_Marsa <c0oler9911@yandex.ru>
2024-01-28 19:15:06 +0300

    now

--- src/ru/progwards/java1/lessons/wrappers/StringConverter.java
+++ src/ru/progwards/java1/lessons/wrappers/StringConverter.java
@@ -4,36 +4,67 @@ public class StringConverter {
     public static String fromByte(byte number){
         Byte numa = number;
         String c = numa.toString();
-        System.out.println(c);
+        System.out.println();
         return c;
     }
-    public static String fromShort(short number){
-        Short numa = number;
-        String c = numa.toString();
-        System.out.println(c);
-        return c;
+    public static String fromShort(long number){
+        String b = fromShort(number);
+        return b;
     }
     public static String fromInt(int number){
-        Integer numa = number;
-        String c = numa.toString();
-        System.out.println(c);
-        return c;}
-    public static String fromLong(long number){
-        Long numa = number;
-        String c = numa.toString();
-        System.out.println(c);
+        String c = fromInt(number);
         return c;
+    }
+    public static String fromLong(long number){
+        String d = fromLong(number);
+        return d;
     }
     public static String fromFloat(float number){
-        Float numa = number;
-        String c = numa.toString();
-        System.out.println(c);
-        return c;
+        String e = fromFloat(number);
+        return e;
     }
     public static String fromDouble(double number){
-        Double numa = number;
-        String c = numa.toString();
-        System.out.println(c);
-        return c;
-    }
+        String f = fromDouble(number);
+        System.out.println();
+        return f;
 }
+
+package ru.progwards.java1.lessons.wrappers;
+
+    public class StringConverter {
+        public static String fromByte(byte number){
+            Byte numa = number;
+            String c = numa.toString();
+            System.out.println(c);
+            return c;
+        }
+        public static String fromShort(short number){
+            Short numa = number;
+            String c = numa.toString();
+            System.out.println(c);
+            return c;
+        }
+        public static String fromInt(int number){
+            Integer numa = number;
+            String c = numa.toString();
+            System.out.println(c);
+            return c;}
+        public static String fromLong(long number){
+            Long numa = number;
+            String c = numa.toString();
+            System.out.println(c);
+            return c;
+        }
+        public static String fromFloat(float number){
+            Float numa = number;
+            String c = numa.toString();
+            System.out.println(c);
+            return c;
+        }
+        public static String fromDouble(double number){
+            Double numa = number;
+            String c = numa.toString();
+            System.out.println(c);
+            return c;
+        }
+    }
