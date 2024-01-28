4a4eb26e679305b19e9fb08c7fbc194242409148
Krab_s_Marsa <c0oler9911@yandex.ru>
2024-01-29 01:47:01 +0300

    now

--- src/ru/progwards/java1/lessons/wrappers/NumMetrics.java
+++ src/ru/progwards/java1/lessons/wrappers/NumMetrics.java
@@ -1,25 +1,29 @@
 package ru.progwards.java1.lessons.wrappers;
 
 public class NumMetrics {
-
+    static int a;
+    static int b;
+    static int c;
+public static Integer Digits(Integer number) {
+int a = number % 100 % 10;
+b = number % 100/10;
+c = number / 100;
+return number;
+}
     public static Integer sumDigits(Integer number) {
-        int a = number % 100 % 10;
-        int b = number % 100/10;
-        int c = number / 100;
+        Digits(number);
         int d = a + b + c;
         return d;
     }
 
     public static Integer mulDigits(Integer number) {
-        int a = number % 100 % 10;
-        int b = number % 100/10;
-        int c = number / 100;
+        Digits(number);
         int d = a * b * c;
         return d;
     }
 
     public static void main(String[] args) {
-        System.out.println(mulDigits(123));
-        System.out.println(sumDigits(123));
+        System.out.println(mulDigits(234));
+        System.out.println(sumDigits(192));
     }
 }
