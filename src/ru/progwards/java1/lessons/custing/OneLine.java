cc0c5b477142f987f359c76a28adf631e60bc7b0
Krab_s_Marsa <c0oler9911@yandex.ru>
2024-02-13 06:41:43 +0300

    now

--- src/ru/progwards/java1/lessons/custing/OneLine.java
+++ src/ru/progwards/java1/lessons/custing/OneLine.java
@@ -4,13 +4,18 @@ public class OneLine {
         return "Передана строка со значением \""+value+"\"";
     }
     public static String intValue(String variable,  int value){
-        return  "Значение "+variable+ " равно "+Integer.toString(value);
+        return  "Значение "+variable+ " равно "+value;
     }
     public static String square(int n){
-        return "Число "+Integer.toString(n)+" в квадрате равно "+Integer.toString((int) Math.pow(n, 2));
+        return "Число "+n+" в квадрате равно "+Math.pow(n, 2);
     }
     public static String sum(int n, int m){
         return "Сумма "+n+" и "+m+ " равна "+(n+m);
     }
-
+    public static void main(String[] args) {
+        System.out.println(strValue("1234"));
+        System.out.println(intValue("opapa",1245));
+        System.out.println(square(5));
+        System.out.println(sum(5,10));
+    }
 }
